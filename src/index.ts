2dbd600aefb2b8803d5b876e6635b4b5b51daa0a
coartem <coartem2@proton.me>
2023-06-22 16:16:02 -0300

    docker

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,12 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    const { message } = req.body
+
+    if (!message)
+        return res.status(400).send({ message: "Message is required"})
+
+    res.send({ message });
 })
 
 app.listen(port, () => {
