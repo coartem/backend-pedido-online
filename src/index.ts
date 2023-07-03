f82fb93f1df494e808b1401e214fac5241ed8d07
coartem <coartem2@proton.me>
2023-07-03 17:20:46 -0300

    endpoint orders

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,54 @@
 import { PrismaClient } from "@prisma/client"
-import dotenv from 'dotenv';
-import express, {Express, Request, Response} from 'express';
+import dotenv from "dotenv"
+import express, { Express, Request, Response } from "express"
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT || 5000;
+const app: Express = express()
+const port = process.env.PORT || 5000
 const prisma = new PrismaClient()
 
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-    const { message } = req.body
+app.get("/", (req: Request, res: Response) => {
+  const { message } = req.body
 
-    if (!message)
-        return res.status(400).send({ message: "Message is required"})
+  if (!message) return res.status(400).send({ error: "Message is required" })
 
-    res.send({ message });
+  res.send({ message })
 })
 
 app.get("/snacks", async (req: Request, res: Response) => {
-    const { snack } = req.query
+  const { snack } = req.query
 
-    if (!snack) return res.status(400).send({ error: "Snack is required" })
+  if (!snack) return res.status(400).send({ error: "Snack is required" })
 
-    const snacks = await prisma.snack.findMany({
-        where: {
-            snack:{
-                equals: snack as string,
-            },
-        },
-    })
+  // SELECT * FROM Snack WHERE snack = 'drink'
+  const snacks = await prisma.snack.findMany({
+    where: {
+      snack: {
+        equals: snack as string,
+      },
+    },
+  })
 
-    res.send(snacks)
+  res.send(snacks)
+})
+
+app.get("/orders/:id", async (req: Request, res: Response) => {
+  const { id } = req.params
+
+  const order = await prisma.order.findUnique({
+    where: {
+      id: +id,
+    },
+  })
+
+  if (!order) return res.status(404).send({ error: "Order not found" })
+
+  res.send(order)
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
