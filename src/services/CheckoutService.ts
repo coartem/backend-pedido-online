319fc4e8fee7cbcece836476e3c3a65cad50c054
coartem <coartem2@proton.me>
2023-07-03 18:16:06 -0300

    CheckoutService

---
+++ src/services/CheckoutService.ts
@@ -0,0 +1,9 @@
+import { CustomerData } from "../interfaces/CustomerData";
+import { PaymentData } from "../interfaces/PaymentData";
+import { SnackData } from "../interfaces/SnackData";
+
+export default class CheckoutService {
+    async process(cart: SnackData[], customer: CustomerData, payment: PaymentData) {
+
+    }
+}
