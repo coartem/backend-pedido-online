e2dbac6707625a48e0c81fcdfbff511b698dc47d
coartem <coartem2@proton.me>
2023-07-03 15:59:38 -0300

    snacks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client"
 import dotenv from 'dotenv';
 import express, {Express, Request, Response} from 'express';
 
@@ -5,6 +6,7 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 5000;
+const prisma = new PrismaClient()
 
 app.use(express.json())
 
@@ -15,6 +17,20 @@ app.get('/', (req: Request, res: Response) => {
         return res.status(400).send({ message: "Message is required"})
 
     res.send({ message });
+})
+
+app.get("/snacks", (req: Request, res: Response) => {
+    const { snack } = req.query
+
+    if (!snack) return res.status(400).send({ error: "Snack is required" })
+
+    const snacks = prisma.client.snack.findMany({
+        where: {
+            snack:{
+                equals: snack as string,
+            },
+        },
+    })
 })
 
 app.listen(port, () => {
